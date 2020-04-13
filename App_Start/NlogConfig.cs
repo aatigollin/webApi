ffe523143918d793da25dd7288e6faf4ef3c4a43
Azilkhan <azilkhan2609@gmai.com>
2020-04-13 16:18:56 +0600

    api + nlog

---
+++ App_Start/NlogConfig.cs
@@ -0,0 +1,27 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webApi.App_Start
+{
+    public static class NlogConfig
+    {
+        public static void Register()
+        {
+
+            //var config = new NLog.Config.LoggingConfiguration();
+
+            // Targets where to log to: File and Console
+            //var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "${basedir}/logs/${shortdate}.log" } ;
+            //logfile.Layout = "${longdate} ${uppercase:${level}} ${message}";
+            
+            // Rules for mapping loggers to targets            
+            //config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+
+            // Apply config           
+            //NLog.LogManager.Configuration = config;
+        }
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace webApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        private readonly NLog.Logger _logger;
+
+        public ValuesController()
+        {
+            _logger = NLog.LogManager.GetCurrentClassLogger();
+        }
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            _logger.Info("get values");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            try
+            {
+                _logger.Info($"get value id = {id}");
+                return "value";
+            }
+            catch(Exception ex)
+            {
+                _logger.Error(ex, "more message");
+                return "";
+            }
+        }
+    }
+}
